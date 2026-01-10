ea596be8cf2d1af33c71b8c2c62baf0080cd7532
Ethan Larner <elarner05@gmail.com>
2026-01-10 19:54:47 +0000

    Added roads class + longest road dfs

--- src/main/java/com/example/model/Road.java
+++ src/main/java/com/example/model/Road.java
@@ -4,6 +4,7 @@ public class Road {
     
     //player who owns this road
     private int playerID;
+    private int buildID;
 
     //array of the two vertices the road connects
     private int[] vertices;
@@ -12,14 +13,16 @@ public class Road {
     public Road(){}
 
     //parameterised constructor
-    public Road(int _playerID, int[] _vertices){
+    public Road(int _playerID, int[] _vertices, int _buildID){
         this.playerID = _playerID;
         this.vertices = _vertices;
+        this.buildID = _buildID;
     }
 
     //getters
     public int getPlayerID(){return this.playerID;}
     public int[] getVertices(){return this.vertices;}
+    public int getBuildID(){return this.buildID;}
 
     //setters
     public void setPlayerID(int _playerID){this.playerID = _playerID;}
